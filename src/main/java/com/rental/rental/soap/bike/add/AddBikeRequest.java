5031149659595c91db2fae08e7c2f75b98af75f1
Kamil <firma_kamil@o2.pl>
2022-06-26 14:52:50 +0200

    Finished

--- src/main/java/com/rental/rental/soap/bike/add/AddBikeRequest.java
+++ src/main/java/com/rental/rental/soap/bike/add/AddBikeRequest.java
@@ -6,59 +6,59 @@ import javax.xml.bind.annotation.*;
 
 /**
  * <p>Java class for anonymous complex type.
- * 
+ *
  * <p>The following schema fragment specifies the expected content contained within this class.
- * 
+ *
  * <pre>
  * &lt;complexType&gt;
  *   &lt;complexContent&gt;
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *     &lt;restriction base="{<a href="http://www.w3.org/2001/XMLSchema">...</a>}anyType"&gt;
  *       &lt;sequence&gt;
- *         &lt;element name="brand" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="make" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
  *         &lt;element name="model" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
  *       &lt;/sequence&gt;
  *     &lt;/restriction&gt;
  *   &lt;/complexContent&gt;
  * &lt;/complexType&gt;
  * </pre>
- * 
- * 
+ *
+ *
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
-    "brand",
-    "model"
+        "make",
+        "model"
 })
 @XmlRootElement(name = "AddBikeRequest", namespace = "https://rental-bike.pl/soap/bike")
 public class AddBikeRequest {
 
     @XmlElement(required = true)
-    protected String brand;
+    protected String make;
     @XmlElement(required = true)
     protected String model;
 
     /**
-     * Gets the value of the brand property.
+     * Gets the value of the make property.
      * 
      * @return
      *     possible object is
      *     {@link String }
      *     
      */
-    public String getBrand() {
-        return brand;
+    public String getMake() {
+        return make;
     }
 
     /**
-     * Sets the value of the brand property.
+     * Sets the value of the make property.
      * 
      * @param value
      *     allowed object is
      *     {@link String }
      *     
      */
-    public void setBrand(String value) {
-        this.brand = value;
+    public void setMake(String value) {
+        this.make = value;
     }
 
     /**
